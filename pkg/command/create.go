1984c940fc5e8348127b9fce9934236c7a0efedf
davidlei <davidleitw@gmail.com>
2021-08-21 01:00:04 +0800

    add create cmd flags

--- pkg/command/create.go
+++ pkg/command/create.go
@@ -17,4 +17,17 @@ var createCmd *cobra.Command = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(createCmd)
+
+	var date string
+	var title string
+	var fulldate string
+	var start, end string
+
+	createCmd.Flags().StringVarP(&title, "title", "t", "", "行程標題 [必填]")
+	createCmd.Flags().StringVarP(&start, "start", "s", "", "開始時間")
+	createCmd.Flags().StringVarP(&end, "end", "e", "", "結束時間")
+	createCmd.Flags().StringVarP(&date, "date", "d", "", "縮寫日期, 格式為: \"04-22\"")
+	createCmd.Flags().StringVarP(&fulldate, "full_date", "D", "", "完整日期, 格式為: \"2019-06-16\"")
+
+	_ = createCmd.MarkFlagRequired("title")
 }
